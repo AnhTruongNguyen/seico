d063a50802667d4807fa35e4dd0f984b7353c0e9
Truong <nguyenanhtruong5350@gmail.com>
2022-11-05 11:33:55 +0700

    up

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,18 @@
 const classToogle = document.querySelector(".menu__toggle");
 const classMenu = document.querySelector(".menu");
-const classShow = "show";
+const classShow = "show";   
 const classOpen = "open";
 classToogle.addEventListener("click", function(){
-    classMenu.classList.toggle(classShow);
-    classToogle.classList.toggle(classOpen);
+    if (window.innerWidth <= 800) {
+        classMenu.classList.toggle(classShow);
+        classToogle.classList.toggle(classOpen);
+    }
 });
+  
+
+    
+
+    
+
+    
+
